3a570d2971baf23ce4da316435426749bd0463f0
Edgar Mancia <eddiemm@users.noreply.github.com>
2020-10-06 12:46:09 -0700

    Add background to input fields for better readability

--- MakeFriendsApp/app/src/main/java/edu/csun/compsci490/makefriendsapp/MainActivity.java
+++ MakeFriendsApp/app/src/main/java/edu/csun/compsci490/makefriendsapp/MainActivity.java
@@ -7,7 +7,6 @@ import android.text.Html;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
