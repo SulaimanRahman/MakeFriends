bb96a92ff00c3481fe523e813837b26a2e12ad7a
SulaimanRahman <60456416+SulaimanRahman@users.noreply.github.com>
2020-10-29 06:58:22 -0700

    Full Name, Picture, and Biography
    getting full name, profile picture and biography should work just fine. when you click on the profile picture, it should allow you to choose a picture, and that picture will be saved in the firebase storage. and it will load that picture on the profile pic icon when you sign back in.

--- MakeFriendsApp/app/src/main/java/edu/csun/compsci490/makefriendsapp/HomeFragment.java
+++ MakeFriendsApp/app/src/main/java/edu/csun/compsci490/makefriendsapp/HomeFragment.java
@@ -1,12 +1,23 @@
 package edu.csun.compsci490.makefriendsapp;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +37,7 @@ public class HomeFragment extends Fragment {
 
     public HomeFragment() {
         // Required empty public constructor
+        Log.d("HomeFagment", "worked constructor");
     }
 
     /**
@@ -43,6 +55,7 @@ public class HomeFragment extends Fragment {
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
+        Log.d("HomeFagment", "worked");
         return fragment;
     }
 
@@ -53,12 +66,19 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Log.d("HomeFagment", "worked");
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+
+
 }
