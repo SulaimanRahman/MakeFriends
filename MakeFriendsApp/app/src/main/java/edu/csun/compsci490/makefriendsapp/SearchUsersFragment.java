e1a741c8c9f26d9d2691124b8ba64f61355a4f2d
Edgar Mancia <eddiemm@users.noreply.github.com>
2020-11-05 18:16:01 -0800

    Add the course spinner for search by course options

--- MakeFriendsApp/app/src/main/java/edu/csun/compsci490/makefriendsapp/SearchUsersFragment.java
+++ MakeFriendsApp/app/src/main/java/edu/csun/compsci490/makefriendsapp/SearchUsersFragment.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SearchUsersFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SearchUsersFragment extends Fragment {
+public class SearchUsersFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -57,7 +64,52 @@ public class SearchUsersFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_search_users, container, false);
+
+        Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner_Course);
+        List<String> courseOptions = new ArrayList<>();
+        courseOptions.add(0, "Course");
+        courseOptions.add("COMP1");
+        courseOptions.add("COMP2");
+        courseOptions.add("COMP3");
+        courseOptions.add("COMP4");
+        courseOptions.add("COMP5");
+
+        ArrayAdapter<String> dataAdapter;
+        dataAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_item, courseOptions);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(dataAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(adapterView.getItemAtPosition(i).equals("Course")){
+                    // do nothing
+                } else {
+                    // on selected spinner item
+                    String item = adapterView.getItemAtPosition(i).toString();
+
+                    // show selected spinner item
+                    Toast.makeText(adapterView.getContext(), "Selected: " + item, Toast.LENGTH_SHORT).show();
+
+                    // search based on item/course selected
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // TODO Auto-generated method stub
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_users, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
