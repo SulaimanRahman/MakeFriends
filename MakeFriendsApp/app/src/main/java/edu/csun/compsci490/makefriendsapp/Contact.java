9a75c5dd29280e26cb7731345fb7e05d94c382cf
nostalgia2021 <nostalgia092@yahoo.com>
2020-11-22 17:42:25 -0800

    contacts page implementation

--- MakeFriendsApp/app/src/main/java/edu/csun/compsci490/makefriendsapp/Contact.java
+++ MakeFriendsApp/app/src/main/java/edu/csun/compsci490/makefriendsapp/Contact.java
@@ -1,43 +1,65 @@
 package edu.csun.compsci490.makefriendsapp;
 
-import android.net.Uri;
 
 public class Contact {
     private String contactName;
-    private String contactEmail;
-    private Uri contactProfilePicUri;
+    private String contactMajor;
+
+    public String getContactBio() {
+        return contactBio;
+    }
+
+    public void setContactBio(String contactBio) {
+        this.contactBio = contactBio;
+    }
+
+    public String getContactInterest() {
+        return contactInterest;
+    }
+
+    public void setContactInterest(String contactInterest) {
+        this.contactInterest = contactInterest;
+    }
+
+    private String contactBio;
+    private String contactInterest;
+
+    public String getUserImg() {
+        return userImg;
+    }
+
+    public void setUserImg(String userImg) {
+        this.userImg = userImg;
+    }
+
+    private String userImg;
 
     public Contact() {
 
     }
 
-    public Contact(String contactName, String contactEmail, Uri contactProfilePicUri) {
+    public Contact(String contactName, String contactMajor, String userImg,String contactBio,String contactInterest) {
         this.contactName = contactName;
-        this.contactEmail = contactEmail;
-        this.contactProfilePicUri = contactProfilePicUri;
+        this.contactMajor = getContactMajor();
+        this.userImg = userImg;
+        this.contactBio = contactBio;
+        this.contactInterest = contactInterest;
     }
 
     public String getContactName() {
         return contactName;
     }
 
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public Uri getContactProfilePicUri() {
-        return contactProfilePicUri;
+    public String getContactMajor() {
+        return contactMajor;
     }
 
     public void setContactName(String contactName) {
         this.contactName = contactName;
     }
 
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
+    public void setContactMajor(String contactEmail) {
+        this.contactMajor = contactEmail;
     }
 
-    public void setContactProfilePicUri(Uri contactProfilePicUri) {
-        this.contactProfilePicUri = contactProfilePicUri;
-    }
 }
